f881f562d908c6881c279dafe046d183fad770ad
brunoguazina <bruno@guazina.com>
2015-03-18 07:25:33 -0400

    interacao do input com o usuario

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,27 @@
 
 
 var pessoa = {}
+var passos = ["Nome", "Idade", "Profissão", "Estado Civil", "Escolaridade"];
 
+var i = 1;
 
 $("#salvar").on('click', function() {
-	console.log('haaaaa');
+		
+	$(".campoDeTexto").attr({
+		id: passos[i],
+		placeholder: passos[i]
+	});;
+	
+	var item = passos[i];
+	console.log(item);
+	pessoa[i]  = $(".campoDeTexto").val();
+
+	incrementar(i);
+
 });
 
+
+function incrementar(){
+	i++;
+}
 
