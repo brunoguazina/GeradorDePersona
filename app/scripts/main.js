2e9918a38dead7c5a9ad423224c8bb1e5ba298d6
brunoguazina <bruno@guazina.com>
2015-03-19 22:04:20 -0400

    reorganizando estrutura do js

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,26 +6,14 @@ var i = 0;
 
 
 $("#salvar").on('click', function() {
-	salvar(itens[i]);
-
-	$(".campoDeTexto").attr({
-		id: itens[i],
-		placeholder: itens[i]
-	});
-		
-	incrementar(i);
-
-	$('.campoDeTexto').val('');
+	salvar(itens[i]);	
+	i++;
+	$("#valorItens").val("");
 });
 
 
 function salvar(item){
-	pessoa[item] =  $('.campoDeTexto').val();
-}
-
-
-function incrementar(){
-	i++;
+	pessoa[item] =  $("#valorItens").val();
 }
 
  
