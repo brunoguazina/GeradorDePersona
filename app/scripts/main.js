e6ff34f9067702613fffbb7e1a622695798ca31d
brunoguazina <bruno@guazina.com>
2015-04-08 22:22:23 -0400

    voltando salvar pessoa na variavel

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,111 +1,86 @@
 
-var pessoa = "";
+var pessoa = {}
 var itens = ["nome", "idade", "profissão", "estado civil", "escolaridade"];
 var totalDeItens = itens.length;
 var i = 0;
 
-
-//$(window).load(function() {
-//
-//	if (localStorage.getItem("Pessoas") == null) {
-//		localStorage.setItem( "Pessoas" ,JSON.stringify([]));
-//	}
-//	pessoas = JSON.parse(localStorage.getItem("Pessoas"));
-//	return pessoas;
-//});
-
-
-//$("#avancar").on("click", function() {
-//
-//	var pessoa = {
-//		nome: $("#valorItens").val()
-//	};
-//
-//	salvar(pessoa);
-//});
-//
-//function salvar(pessoa) {
-//	pessoas.push(pessoa);
-//	localStorage.setItem( "Pessoas" ,JSON.stringify(pessoas));
-//	location.reload();
-//}
-
+(function(){
+	"use strict";
 		
-$("#valorItens").focus();
+	$("#valorItens").focus();
 
+	$("#voltar").on('click', function() {
+		salvar(itens[i]);
 
-$("#voltar").on('click', function() {
- 	salvar(itens[i]);
+		if(i > 0) {
+			decrementar();
+		}
 
- 	if(i > 0) {
- 		decrementar();
+		tratarMensagem(itens[i]);
+		pegarValor(itens[i]);
+		status();
+		$("#valorItens").focus();
+	});
+
+	$("#avancar").on('click', function() {
+		salvar(itens[i]);
+
+		if (i < totalDeItens){
+			incrementar();
+		} if (i === totalDeItens) {
+			$(".dialogo").show();
+			$(".nomePersona").empty().append(pessoa.nome + " - " + pessoa.profissão);
+		}
+		tratarMensagem(itens[i]);
+		$("#valorItens").val("");
+		status();
+		$("#valorItens").focus();
+	});
+
+	$(".fecharDialogo").on('click', function() {
+		$(".dialogo").toggle();
+	});
+
+	$(".persona").on("click", function(){
+		window.location.pathname = "persona.html";
+		
+	});
+
+	function montarPersona(){
+         $(".nome").html("Some text and markup");
+      }
+
+	$( document ).ready(function() {
+        $(".nome").html("Bruno Guazina");
+        $(".profissao").html(pessoa.profissao);
+    });
+
+	function pegarValor(item) {
+		console.log(pessoa[item]);
+		$("#valorItens").val(pessoa[item]);
 	}
 
- 	tratarMensagem(itens[i]);
- 	pegarValor(itens[i]);
- 	status();
- 	$("#valorItens").focus();
-});
-
-
-$("#avancar").on('click', function() {
-	salvar(itens[i]);
-
-	if (i < totalDeItens){
-		incrementar();
-	} if (i === totalDeItens) {
-		$(".dialogo").show();
-		$(".nomePersona").empty().append(pessoa.nome + " - " + pessoa.profissão);
+	function salvar(item){
+		pessoa[item] = $("#valorItens").val();
 	}
-	tratarMensagem(itens[i]);
-	$("#valorItens").val("");
-	status();
-});
 
+	function tratarMensagem(item) {
+		$("#nomeDoItem").html(item);
+		$("#valorItens").attr('placeholder', item);
+	}
 
-$(".fecharDialogo").on('click', function() {
-	$(".dialogo").toggle();
-});
+	function status() {
+		var valorDeCadaItem = i/totalDeItens;
+		var resultado = valorDeCadaItem * 100;
+		$("#barraDeProgresso").css( "width", resultado + "%");
+	}
 
-$(".persona").on("click", function(){
-	window.location.pathname = "persona.html";
-	
-});
+	function incrementar() {
+		i++;
+	}
 
-function montarPersona(){
-	$(".nome").html("Some text and markup");
-}
+	function decrementar() {
+		i--;
+	}
 
-$( document ).ready(function() {
-     $(".nome").html("Bruno Guazina");
-     $(".profissao").html(pessoa.profissao);
-});
-
-function pegarValor(item) {
-	console.log(pessoa[item]);
-	$("#valorItens").val(pessoa[item]);
-}
-
-function salvar(item){
- 	pessoa[item] = $("#valorItens").val();
-}
-
-function tratarMensagem(item) {
-	$("#nomeDoItem").html(item);
-	$("#valorItens").attr('placeholder', item);
-}
-
-function status() {
-	var valorDeCadaItem = i/totalDeItens;
-	var resultado = valorDeCadaItem * 100;
-	$("#barraDeProgresso").css( "width", resultado + "%");
-}
-
-function incrementar() {
-	i++;
-}
-
-function decrementar() {
-	i--;
-}
-
+})();
